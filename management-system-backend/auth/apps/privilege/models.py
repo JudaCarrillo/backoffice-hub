242ad923e885904edcb59f6bf352eebc7c84feea
Juda Carrillo <130939221+JudaCarrillo@users.noreply.github.com>
2024-04-08 23:02:55 -0500

    feat: upd

--- management-system-backend/auth/apps/privilege/models.py
+++ management-system-backend/auth/apps/privilege/models.py
@@ -8,4 +8,7 @@ class Privilege(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
+    title = models.CharField(max_length=100, null=True)
+    route= models.CharField(max_length=100, null=True)
+    icon = models.CharField(max_length=50, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
