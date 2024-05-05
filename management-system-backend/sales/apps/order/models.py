56bed358ee8ad24465d7a421b0b116be59edf1ab
Juda <130939221+JudaCarrillo@users.noreply.github.com>
2024-05-05 14:56:37 -0500

    feat: upd

--- management-system-backend/sales/apps/order/models.py
+++ management-system-backend/sales/apps/order/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Shippers(models.Model):
+    id = models.AutoField(primary_key=True)
+    company_name = models.CharField(max_length=40)
+    phone = models.CharField(max_length=24)
+
+
+class Orders(models.Model):
+    id = models.AutoField(primary_key=True)
+    order_date = models.DateField()
+    required_date = models.DateField()
+    shipped_date = models.DateField()
+    freight = models.DecimalField(max_digits=10, decimal_places=2)
+    ship_name = models.CharField(max_length=40)
+    ship_address = models.CharField(max_length=60)
+    ship_city = models.CharField(max_length=15)
+    ship_region = models.CharField(max_length=15)
+    ship_postal_code = models.CharField(max_length=10)
+    ship_country = models.CharField(max_length=15)
+    employee_id = models.IntegerField(db_index=True)
+    customer_id = models.CharField(max_length=5, db_index=True)
+    ship_via = models.ForeignKey('Shippers', on_delete=models.CASCADE)
--- management-system-backend/sales/apps/order_detail/apps.py
+++ management-system-backend/sales/apps/order_detail/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class OrderDetailConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'order_detail'
+    name = 'apps.order_detail'
