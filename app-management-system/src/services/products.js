4640aa53407a3c5400ccea7cfc74acdae8fe2242
Juda <130939221+JudaCarrillo@users.noreply.github.com>
2024-05-05 01:15:27 -0500

    feat: upd

--- app-management-system/src/services/products.js
+++ app-management-system/src/services/products.js
@@ -2,30 +2,30 @@ import axios from "axios";
 
 const url_base = process.env.API_BASE_URL_CATALOG;
 const apiVersion = process.env.API_VERSION;
-const subject = "product";
+const subject = "products";
 
 export const getProducts = () => {
-  return axios.get(`${url_base}${apiVersion}/${subject}/`);
+  return axios.get(`${url_base}/${apiVersion}/${subject}/`);
 };
 
 export const exportProductsToCsv = () => {
-  return axios.get(`${url_base}${apiVersion}/${subject}/export`, {
+  return axios.get(`${url_base}/${apiVersion}/${subject}/export`, {
     responseType: "blob",
   });
 };
 
 export const deleteProduct = (id) => {
-  return axios.delete(`${url_base}${apiVersion}/${subject}/delete/${id}`);
+  return axios.delete(`${url_base}/${apiVersion}/${subject}/delete/${id}`);
 };
 
 export const createProduct = (data) => {
-  return axios.post(`${url_base}${apiVersion}/${subject}/create`, data);
+  return axios.post(`${url_base}/${apiVersion}/${subject}/create`, data);
 };
 
 export const updateProduct = (id, data) => {
-  return axios.put(`${url_base}${apiVersion}/${subject}/update/${id}`, data);
+  return axios.put(`${url_base}/${apiVersion}/${subject}/update/${id}`, data);
 };
 
 export const getProductsId = (id) => {
-  return axios.get(`${url_base}${apiVersion}/${subject}/${id}`);
+  return axios.get(`${url_base}/${apiVersion}/${subject}/${id}`);
 };
